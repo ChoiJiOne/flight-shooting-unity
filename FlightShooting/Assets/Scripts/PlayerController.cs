dd562da92fb83c812668ff05fbc66b4f5e1aaa08
CHOIJIONE\choij <choijione02@gmail.com>
2025-07-20 08:23:49 +0900

    플레이어가 스테이지 밖으로 나가지 못하는 기능 추가

--- FlightShooting/Assets/Scripts/PlayerController.cs
+++ FlightShooting/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private StageData _stageData;
     [SerializeField] private MovementController _movementController;
 
     private void Update()
@@ -10,5 +11,13 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
 
         _movementController.MoveTo(new Vector3(x, y, 0.0f));
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, _stageData.LimitMin.x, _stageData.LimitMax.x),
+            Mathf.Clamp(transform.position.y, _stageData.LimitMin.y, _stageData.LimitMax.y)
+        );
     }
 }
