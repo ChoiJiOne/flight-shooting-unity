e30affb2e6b0eb7de236f4fb9004359ae07b51c5
CHOIJIONE\choij <choijione02@gmail.com>
2025-07-20 08:34:05 +0900

    배경 스크롤 기능 추가

---
+++ FlightShooting/Assets/Scripts/PositionScroller.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PositionScroller : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _scrollRange = 9.9f;
+    [SerializeField] private float _moveSpeed = 3.0f;
+    [SerializeField] private Vector3 _moveDirection = Vector3.down;
+
+    private void Update()
+    {
+        transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
+
+        if (transform.position.y <= -_scrollRange)
+        {
+            transform.position = _target.position + Vector3.up * _scrollRange;
+        }
+    }
+}
