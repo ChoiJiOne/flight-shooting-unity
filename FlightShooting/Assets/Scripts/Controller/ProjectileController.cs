937d6ebd38950fd1d47cf134986abee2c237ea02
CHOIJIONE\choij <choijione02@gmail.com>
2025-07-21 07:48:58 +0900

    플레이어가 적 공격 시 깜빡이는 효과 연출 기능 추가

--- FlightShooting/Assets/Scripts/Controller/ProjectileController.cs
+++ FlightShooting/Assets/Scripts/Controller/ProjectileController.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
+    [SerializeField] private int _damage = 1;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            collision.GetComponent<EnemyController>().OnDie();
+            collision.GetComponent<EnemyHp>().TakeDamage(_damage);
+
             Destroy(gameObject);
         }
     }
---
+++ FlightShooting/Assets/Scripts/EnemyHp.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyHp : MonoBehaviour
+{
+    [SerializeField] private float _maxHp = 4.0f;
+    [SerializeField] private EnemyController _enemyController;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    private float _currentHp;
+
+    private void Awake()
+    {
+        _currentHp = _maxHp;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHp -= damage;
+
+        StopCoroutine(nameof(HitColorAnimation));
+        StartCoroutine(nameof(HitColorAnimation));
+
+        if (_currentHp < 0.0f)
+        {
+            _enemyController.OnDie();
+        }
+    }
+
+    private IEnumerator HitColorAnimation()
+    {
+        _spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.05f);
+        _spriteRenderer.color = Color.white;
+    }
+}
