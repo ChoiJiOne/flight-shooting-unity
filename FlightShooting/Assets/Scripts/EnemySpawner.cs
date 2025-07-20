61c057917cc7dc108e2a07031e069acae20c1952
CHOIJIONE\choij <choijione02@gmail.com>
2025-07-21 07:49:21 +0900

    적 생성 시 적의 HP UI 오브젝트 생성 기능 추가

--- FlightShooting/Assets/Scripts/EnemySpawner.cs
+++ FlightShooting/Assets/Scripts/EnemySpawner.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Header("Ingame Enemy")]
     [SerializeField] private StageData _stageData;
     [SerializeField] private GameObject _enemyPrefab;
     [SerializeField] private float _spawnTime;
+
+    [Header("Enemy UI")]
+    [SerializeField] private GameObject _enemyHpPrefab;
+    [SerializeField] private Transform _canvasTransform;
 
     private void Awake()
     {
@@ -17,8 +22,19 @@ public class EnemySpawner : MonoBehaviour
         while (true)
         {
             float positionX = Random.Range(_stageData.LimitMin.x, _stageData.LimitMax.x);
-            Instantiate(_enemyPrefab, new Vector3(positionX, _stageData.LimitMax.y + 1.0f, 0.0f), Quaternion.identity);
+            Vector3 position = new Vector3(positionX, _stageData.LimitMax.y + 1.0f, 0.0f);
+
+            GameObject enemyClone = Instantiate(_enemyPrefab, position, Quaternion.identity);
+            SpawnEnemyHp(enemyClone);
+
             yield return new WaitForSeconds(_spawnTime);
         }
+    }
+
+    private void SpawnEnemyHp(GameObject enemy)
+    {
+        GameObject enemyHpClone = Instantiate(_enemyHpPrefab);
+        enemyHpClone.transform.SetParent(_canvasTransform);
+        enemyHpClone.transform.localScale = Vector3.one;
     }
 }
