166b2701a26d23753fe8332ef8b9652ef98fc308
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-07-20 10:55:23 +0900

    플레이어의 데미지 타격 효과를 받도록 표시하는 기능 추가

---
+++ FlightShooting/Assets/Scripts/Effect/DamageEffect.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class DamageEffect : MonoBehaviour
+{
+    [SerializeField] private float _maxHp = 10.0f;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
+    private float _currentHp = 0.0f;
+
+    private void Awake()
+    {
+        _currentHp = _maxHp;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHp -= damage;
+
+        StopCoroutine(nameof(HitColorAnimation));
+        StartCoroutine(nameof(HitColorAnimation));
+
+        if (_currentHp <= 0.0f)
+        {
+            Debug.Log($"Player HP: {_currentHp}... Die");
+        }
+    }
+
+    private IEnumerator HitColorAnimation()
+    {
+        _spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        _spriteRenderer.color = Color.white;
+    }
+}
