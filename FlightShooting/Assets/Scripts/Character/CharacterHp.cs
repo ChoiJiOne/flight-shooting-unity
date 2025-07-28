75fb0156b55a0f26a9fd6da6f1509f9559385641
CHOIJIONE\choij <choijione02@gmail.com>
2025-07-29 07:53:17 +0900

    인게임 내의 캐릭터 HP를 관리하는 스크립트 추가

---
+++ FlightShooting/Assets/Scripts/Character/CharacterHp.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+
+public class CharacterHp : MonoBehaviour
+{
+    public float MaxHp => _maxHp;
+    public float CurrentHp
+    {
+        get => _currentHp;
+        set => _currentHp = Mathf.Clamp(value, 0, _maxHp);
+    }
+
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Color _hitColor;
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private float _damageFlashDuration = 0.1f;
+
+    private float _maxHp;
+    private float _currentHp;
+    private ICharacterController _characterController;
+
+    public void Init(float maxHp, ICharacterController characterController)
+    {
+        _maxHp = Mathf.Clamp(maxHp, 1, maxHp);
+        _currentHp = _maxHp;
+        _characterController = characterController;
+    }
+
+    public void TakeDagame(float damage)
+    {
+        _currentHp -= damage;
+
+        StopCoroutine(nameof(HitColorAnimation));
+        StartCoroutine(nameof(HitColorAnimation));
+
+        if (_currentHp <= 0.0f)
+        {
+            _characterController.OnDie();
+        }
+    }
+
+    private IEnumerator HitColorAnimation()
+    {
+        _spriteRenderer.color = _hitColor;
+        yield return new WaitForSeconds(_damageFlashDuration);
+        _spriteRenderer.color = _defaultColor;
+    }
+}
